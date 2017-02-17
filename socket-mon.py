4b20cdba46224401d1858b6b1a1022c7e645fd96
swathijasti <swathi.lbs16@gmail.com>
2017-02-17 13:27:41 +0530

    Add files via upload
    socketmon

---
+++ socket-mon.py
@@ -0,0 +1,19 @@
+import psutil
+import collections
+import operator
+import csv
+import sys
+k=psutil.net_connections()
+#print k
+out = []
+print ("pid,laddr,raddr,status")
+for i in k:
+    if (i[3] and i[4]):
+        out.append([i[6],i[3],i[4]s,i[5]])
+out.sort()
+#print ', '.join(['"'+i+'"' for i in out])
+for i in out:
+    x= i[0]
+    #print i
+    print i[0],i[1],i[2],i[3]
+    x=i[0]
